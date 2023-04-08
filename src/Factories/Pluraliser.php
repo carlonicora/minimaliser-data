0eedca4a1c6839854494b68331ede62225392119
Carlo Nicora <carlo.nicora@gmail.com>
2023-04-08 11:46:46 +0200

    fix: correct pluraliser

--- src/Factories/Pluraliser.php
+++ src/Factories/Pluraliser.php
@@ -12,8 +12,12 @@ class Pluraliser
             $response = substr($name, 0, -3) . 'y';
         } elseif (strtolower(substr($name, strlen($name)-4)) === 'sses') {
             $response = substr($name, 0, -2);
+        } elseif (strtolower(substr($name, strlen($name)-4)) === 'zzes') {
+            $response = substr($name, 0, -3);
         } elseif (strtolower(substr($name, strlen($name)-3)) === 'ses') {
-            $response = substr($name, 0, -1);
+            $response = substr($name, 0, -2);
+        } elseif (strtolower(substr($name, strlen($name)-2)) === 'es') {
+            $response = substr($name, 0, -2);
         } elseif (strtolower(substr($name, strlen($name)-1)) === 's') {
             $response = substr($name, 0, -1);
         }
@@ -28,6 +32,8 @@ class Pluraliser
 
         if (strtolower(substr($name, strlen($name)-1)) === 'y') {
             $response = substr($response, 0, -1) . 'ies';
+        } elseif (strtolower(substr($name, strlen($name)-2)) === 'iz') {
+            $response = $response . 'zes';
         } elseif (strtolower(substr($name, strlen($name)-1)) !== 's') {
             $response = $name . 's';
         }
